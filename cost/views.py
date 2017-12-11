dc0d377297197bf60cfb6d592f3af70d32bd5e9e
Pawel Zalewski <philusky@gmail.com>
2017-12-11 17:22:32 +0100

    first algo

--- cost/views.py
+++ cost/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from collections import namedtuple
 
 from .forms import CmpForm
+
+task = namedtuple('Task'['start', 'end', 't-n', 't-gr', 'K-n', 'K-gr'])
 
 def index(request):
     if request.method == 'POST':
@@ -23,3 +26,32 @@ def result(request):
     return render(request, 'cost/result.html',
                   {'data': data}
                   )
+
+def find_critical_paths(tasks):
+    
+    nodes = []    
+
+    unfinished_paths = []    
+
+    tasks = sorted(tasks, key=lambda x: x.a, reverse=True)
+
+
+    path = [tasks[0]]
+    for taskk in tasks:
+	# jesli koniec itemu jest poczatkiem ostatniego w path
+    	if taskk.end == path[[-1].start:
+	    # zobacz, czy nie ma innych w tej samej linii
+	    brothers = []
+            number_of_brothers = 0
+	        for new_task in tasks:
+                    if new_task.end == taskk.end and new_task is not taskk:
+                        number_of_brothers += 1
+		        brothers.append(new_task)
+            if number_of_brothers != 0:
+                for brother in brothers:
+		    copy_path = list(path)
+		    copy_path.append(brother)
+		    unfinished_paths.append(copy_path)
+	    path.append(taskk)
+                
+        
