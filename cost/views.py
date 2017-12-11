f4624ea68238bef0aa1e2d95e31db7e1b4dc54cb
Pawel Zalewski <philusky@gmail.com>
2017-12-11 21:34:16 +0100

    algo, chuj wi, czy dobre (#1)
    * first algo
    
    * Algorytm

--- cost/views.py
+++ cost/views.py
@@ -4,6 +4,30 @@ from django.urls import reverse
 
 from .forms import CmpForm
 
+class Task(object):
+    def __init__(self, start, end, tn, tgr, Kn, Kgr):
+        self.start = start
+        self.end = end
+        self.tn = tn
+        self.tgr = tgr
+        self.Kn = Kn
+        self.Kgr = Kgr
+        self.reduced = False
+        self.saved = 0
+        if self.tn == self.tgr:
+            self.gradient = 0
+        else:
+            self.gradient = (self.Kgr - self.Kn) / (self.tn - self.tgr)
+
+    def __repr__(self):
+        return "Task(%s,%s,%s,%s,%s,%s,%s)" % (self.start,
+        self.end,
+        self.tn ,
+        self.tgr,
+        self.Kn,
+        self.Kgr,
+        self.gradient)
+
 def index(request):
     if request.method == 'POST':
         form = CmpForm(request.POST)
@@ -19,7 +43,79 @@ def index(request):
 def result(request):
     data = request.session.get('_data')
 
-    # Todo: tu dodac logike obliczen w data masz wszystie elementy ktore zdefiniowales w forms.py
+    #przyklad inputu
+    lista = [Task(1, 2, 8, 8, 220, 280),
+             Task(1, 4, 10, 5, 100, 150),
+             Task(2, 3, 6, 4, 300, 400),
+             Task(3, 6, 12, 10, 260, 300),
+             Task(4, 5, 15, 15, 150, 150),
+             Task(5, 6, 10, 2, 200, 360),
+             ]
+
+    save = 0
+
+    # wywolanie funkcji
+    reduce(lista)
+    for task in lista:
+        save += task.saved
+    # output - kosztu przyspieszenia
+    print save
+
     return render(request, 'cost/result.html',
                   {'data': data}
                   )
+
+def find_critical_paths(tasks):
+    tasks = sorted(tasks, key=lambda x: x.end, reverse=True)
+
+    brothers = [tasks[0]]
+
+    critical_paths = []                                  
+
+    paths = [[tasks[0]]]
+    for path in paths:
+        orig_path = path
+        
+        for taskk in tasks:
+            if taskk is path[-1]:
+                next
+            if taskk.end == path[-1].end and taskk not in brothers:
+                if len(orig_path) > 1:
+                    paths.append(orig_path[:-1].append(taskk))
+                else:
+                    paths.append([taskk])
+                brothers.append(taskk)
+                next
+    	    if taskk.end == path[-1].start:
+           	    path.append(taskk)
+               
+    miin = 9999999999999
+    for path in paths:
+        time = 0
+        for task in path:
+            time += task.tn
+        if time < miin:
+            critical_paths = [path]
+        elif time == miin:
+            critical_paths.append(path)
+
+    return critical_paths
+
+
+def reduce(listt):
+    nodes = []
+    crit_paths = find_critical_paths(listt)
+    print crit_paths
+    for path in crit_paths:
+        for task in path:
+            nodes.append(task)
+
+    tasks_by_gradient = sorted(nodes, key=lambda x: x.gradient, reverse=False)
+    for task in tasks_by_gradient:
+        if task.gradient != 0 and task.reduced is False:
+            task.reduced = True
+            task.saved = (task.tn - task.tgr) * task.gradient
+            task.tn = task.tgr
+            reduce(listt)
+
+    return listt
