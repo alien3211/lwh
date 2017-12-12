d3b6e7203652984a9d292a30095f1aede3a67544
Zalewski, Pawel <pawel.zalewski@sabre.com>
2017-12-12 09:32:35 +0100

    poprawki

--- cost/forms.py
+++ cost/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 class CmpForm(forms.Form):
-    cos = forms.CharField(label='Your name', max_length=100)
-    tam = forms.CharField(label='tam', max_length=100)
+    polaczenie = forms.CharField(label='Polaczenie', max_length=100)
+    czas = forms.CharField(label='Czas', max_length=100)
+    koszt = forms.CharField(label='Koszt', max_length=100)
     # ToDo: tu masz dodac potrzebne ci parametry plus validacja jak potrzebujesz liczby to liczby a nie stringi
