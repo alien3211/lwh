a558c5ba8f88995f7f9ed4851283d8079c73e141
Zalewski, Pawel <pawel.zalewski@sabre.com>
2017-12-12 09:40:37 +0100

    literowka

--- cost/forms.py
+++ cost/forms.py
@@ -1,6 +1,6 @@
-form django import forms
- 
+from django import forms
+
 class CmpForm(forms.Form):
     cos = forms.CharField(label='Your name', max_length=100)
     tam = forms.CharField(label='tam', max_length=100)
-    # ToDo: tu masz dodac potrzebne ci parametry plus validacja jak potrzebujesz liczby to liczby a nie stringi 
+    # ToDo: tu masz dodac potrzebne ci parametry plus validacja jak potrzebujesz liczby to liczby a nie stringi
