3c1808f743675d047e22ccb0e36fd86da16a9b43
Tetich Alan <alan.tetich.ext@nokia-sbell.com>
2017-12-14 19:59:01 +0100

    update forms, results. Add beter show result

--- cost/forms.py
+++ cost/forms.py
@@ -1,6 +1,103 @@
 from django import forms
+from django.forms.formsets import BaseFormSet
 
 class CmpForm(forms.Form):
-    cos = forms.CharField(label='Your name', max_length=100)
-    tam = forms.CharField(label='tam', max_length=100)
-    # ToDo: tu masz dodac potrzebne ci parametry plus validacja jak potrzebujesz liczby to liczby a nie stringi
+    start = forms.FloatField(
+                    widget=forms.TextInput(attrs={
+                        'placeholder': 'start',
+                    }),
+                    required=False)
+    end = forms.FloatField(
+                    widget=forms.TextInput(attrs={
+                        'placeholder': 'end',
+                    }),
+                    required=False)
+    tn = forms.FloatField(
+                    widget=forms.TextInput(attrs={
+                        'placeholder': 'tn',
+                    }),
+                    required=False)
+    tgr = forms.FloatField(
+                    widget=forms.TextInput(attrs={
+                        'placeholder': 'tgr',
+                    }),
+                    required=False)
+    Kn = forms.FloatField(
+                    widget=forms.TextInput(attrs={
+                        'placeholder': 'Kn',
+                    }),
+                    required=False)
+    Kgr = forms.FloatField(
+                    widget=forms.TextInput(attrs={
+                        'placeholder': 'Kgr',
+                    }),
+                    required=False)
+
+class LinkForm(forms.Form):
+    # cos = forms.CharField(label='Your name', max_length=100)
+    # tam = forms.CharField(label='tam', max_length=100)
+    # # ToDo: tu masz dodac potrzebne ci parametry plus validacja jak potrzebujesz liczby to liczby a nie stringi
+
+    """
+    Form for individual user links
+    """
+    anchor = forms.CharField(
+                    max_length=100,
+                    widget=forms.TextInput(attrs={
+                        'placeholder': 'Link Name / Anchor Text',
+                    }),
+                    required=False)
+    url = forms.URLField(
+                    widget=forms.URLInput(attrs={
+                        'placeholder': 'URL',
+                    }),
+                    required=False)
+
+
+
+# validation data
+class BaseLinkFormSet(BaseFormSet):
+    def clean(self):
+        """
+        Adds validation to check that no two links have the same anchor or URL
+        and that all links have both an anchor and URL.
+        """
+        if any(self.errors):
+            return
+
+        anchors = []
+        urls = []
+        duplicates = False
+
+        for form in self.forms:
+            if form.cleaned_data:
+                anchor = form.cleaned_data['anchor']
+                url = form.cleaned_data['url']
+
+                # Check that no two links have the same anchor or URL
+                if anchor and url:
+                    if anchor in anchors:
+                        duplicates = True
+                    anchors.append(anchor)
+
+                    if url in urls:
+                        duplicates = True
+                    urls.append(url)
+
+                if duplicates:
+                    raise forms.ValidationError(
+                        'Links must have unique anchors and URLs.',
+                        code='duplicate_links'
+                    )
+
+                # Check that all links have both an anchor and URL
+                if url and not anchor:
+                    raise forms.ValidationError(
+                        'All links must have an anchor.',
+                        code='missing_anchor'
+                    )
+                elif anchor and not url:
+                    raise forms.ValidationError(
+                        'All links must have a URL.',
+                        code='missing_URL'
+                    )
--- cost/views.py
+++ cost/views.py
@@ -1,8 +1,12 @@
+from collections import OrderedDict
+
+from django.forms import formset_factory
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
 from .forms import CmpForm
+
 
 class Task(object):
     def __init__(self, start, end, tn, tgr, Kn, Kgr):
@@ -28,39 +32,29 @@ class Task(object):
         self.Kgr,
         self.gradient)
 
-def index(request):
-    if request.method == 'POST':
-        form = CmpForm(request.POST)
-        if form.is_valid():
-            request.session['_data'] = request.POST
-            return HttpResponseRedirect(reverse('result'))
-    else:
-        form = CmpForm()
-    return render(request, 'cost/cmp_cost.html',
-                  {'form': form}
-                  )
+    def to_dict(self):
+        return OrderedDict(
+            ('start', self.start),
+            ('end', self.end),
+            ('tn', self.tn),
+            ('tgr', self.tgr),
+            ('Kn', self.Kn),
+            ('Kgr', self.Kgr),
+            ('gradient', self.gradient)
+        )
+
 
 def result(request):
     data = request.session.get('_data')
 
-    #przyklad inputu
-    lista = [Task(1, 2, 8, 8, 220, 280),
-             Task(1, 4, 10, 5, 100, 150),
-             Task(2, 3, 6, 4, 300, 400),
-             Task(3, 6, 12, 10, 260, 300),
-             Task(4, 5, 15, 15, 150, 150),
-             Task(5, 6, 10, 2, 200, 360),
-             ]
-
-    save = 0
-
-    # wywolanie funkcji
-    # *wynik*: czynnosci po redukcji
-    wynik = reduce(lista)
-    for task in wynik:
-        save += task.saved
-    # *save*: laczne koszty przyspieszenia
-    print(save)
+    # #przyklad inputu
+    # lista = [Task(1, 2, 8, 8, 220, 280),
+    #          Task(1, 4, 10, 5, 100, 150),
+    #          Task(2, 3, 6, 4, 300, 400),
+    #          Task(3, 6, 12, 10, 260, 300),
+    #          Task(4, 5, 15, 15, 150, 150),
+    #          Task(5, 6, 10, 2, 200, 360),
+    #          ]
 
     return render(request, 'cost/result.html',
                   {'data': data}
@@ -105,11 +99,60 @@ def find_critical_paths(tasks):
     return critical_paths
 
 
-def reduce(listt):
+
+def index(request):
+    """
+    Allows a user to update their own profile.
+    """
+
+    # Create the formset, specifying the form and formset we want to use.
+    LinkFormSet = formset_factory(CmpForm)
+
+
+    if request.method == 'POST':
+
+        link_formset = LinkFormSet(request.POST)
+
+        if  link_formset.is_valid():
+
+            list_tasks = []
+            for link_form in link_formset:
+                start = link_form.cleaned_data.get('start')
+                end = link_form.cleaned_data.get('end')
+                tn = link_form.cleaned_data.get('tn')
+                tgr = link_form.cleaned_data.get('tgr')
+                Kn = link_form.cleaned_data.get('Kn')
+                Kgr = link_form.cleaned_data.get('Kgr')
+                list_tasks.append(Task(start, end, tn, tgr, Kn, Kgr))
+
+            save = 0
+            critical_paths = []
+            wynik = reduce(list_tasks, critical_paths)
+            for task in wynik:
+                save += task.saved
+
+            request.session['_data'] = {
+                'save': save,
+                'critical_paths': [list(map(vars, x[0])) for x in critical_paths], # get all vars from task object [[[],[]], [[],[]]]
+                'keys': ['start', 'end', 'tn', 'tgr', 'Kn', 'Kgr', 'gradient']
+            }
+            return HttpResponseRedirect(reverse('result'))
+
+    else:
+        link_formset = LinkFormSet()
+
+    context = {
+        'link_formset': link_formset,
+    }
+
+    return render(request, 'cost/cmp_cost.html', context)
+
+def reduce(listt, critical_paths):
     # print("Enter!")
     nodes = []
     crit_paths = find_critical_paths(listt)
-    # print(listt)
+    critical_paths.append(crit_paths)
+    print(crit_paths)
     # print(crit_paths)
     # print("------------------")
     for path in crit_paths:
@@ -123,7 +166,7 @@ def reduce(listt):
             task.reduced = True
             task.saved = (task.tn - task.tgr) * task.gradient
             task.tn = task.tgr
-            reduce(listt)
+            reduce(listt, critical_paths)
             break
 
     return listt
